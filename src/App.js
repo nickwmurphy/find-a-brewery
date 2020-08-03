0946f48d9e69ba2954ea1ef93195c5350c663804
Nick Murphy <nickmurphy@Nick-Murphys-MBP.local>
2020-08-02 23:46:56 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Select } from "@procore/core-react";
+import styles from "./App.module.scss";
 
 function App() {
+  const [list, setList] = React.useState([]);
+  const [value, setValue] = React.useState({ id: null, name: null });
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+
+  const onSelect = ({ item }) => {
+    setValue(item);
+  };
+
+  const onClear = () => {
+    setValue({});
+  };
+
+  const url = "https://api.openbrewerydb.org/breweries?per_page=50";
+
+  const getBreweries = async () => {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    return data;
+  };
+
+  const beforeShow = async () => {
+    setLoading(true);
+    return await getBreweries().then((data) => {
+      setLoading(false);
+      setList(data);
+      return true;
+    });
+  };
+
+  const afterHide = () => {
+    setError(false);
+  };
+
+  const breweries = list.map((item) => (
+    <Select.Option
+      className={styles.capitalize}
+      key={item.id}
+      value={item}
+      selected={item.id === value.id}
+    >
+      {item.name}
+    </Select.Option>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Select
+      afterHide={afterHide}
+      beforeShow={beforeShow}
+      error={error}
+      label={value.name}
+      loading={loading}
+      onClear={onClear}
+      onSelect={onSelect}
+      placeholder="Select an item"
+    >
+      {breweries}
+    </Select>
   );
 }
 
